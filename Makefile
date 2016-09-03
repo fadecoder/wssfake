90661775d17c2cbbf3368cafef7b978ee0565091
wcg <wcg@linux.com>
2016-09-03 14:36:45 +0800

    visit every 8 byte in normal.c

--- Makefile
+++ Makefile
@@ -2,9 +2,9 @@ fake:wss_fake.c
 	 gcc -O2 -o wss_fake wss_fake.c
 s:wss_fake_stage.c
 	 gcc -O2 -o wss_fake wss_fake_stage.c
-	 cp wss_fake /wss_fake -f
 n:wss_fake_normal.c
 	 gcc -O2 -o wss_fake wss_fake_normal.c
-	 cp wss_fake /wss_fake -f
+m:wss_fake_normal.c
+	 gcc -o wss_fake wss_fake_normal.c
 clean:
 	rm wss_fake -f
--- wss_fake_normal.c
+++ wss_fake_normal.c
@@ -8,19 +8,31 @@
 typedef long unsigned un_long;
 
 int main(int argc, char **argv){
-	un_long *array, i = 0;
-	int j, ch, array_size = 1000;
+    un_long *array, i = 0;
+    int j, ch, array_size = 1000;
+    int round = 100;
+    int cntw=0;
+
+    if(argc > 1){
+	array_size=atoi(argv[1]);
+	round = atoi(argv[2]);
+    }
 
     int len = array_size * M;	
-    int round = 100;
     int maxinter = len / sizeof(un_long);
     array = (un_long *)malloc(len);
-		printf("fail\n");
-	for(j=0; j < round; j++){
-		for(i=0 ; i < maxinter; i = i + 512){
-	//		*(array + i)  = i;
+    if(NULL == array){
+	printf("fail\n");
+    }
+    printf("array_size=%d round=%d\n",array_size,round);
+    for(j=0; j < round; j++){
+//		printf("cnt=%d\n",j);
+		for(i=0 ; i < maxinter; i = i + 1){
+			*(array + i)  = i;
+			cntw++;
 		}
-	}
+    }
+    printf("cntw=%d\n",cntw);
     free(array);
 }
 
